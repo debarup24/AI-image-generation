7bfbc07b3937a456c926e6c900267484809f0df0
Debarup <87480898+debarup24@users.noreply.github.com>
2025-04-05 16:58:45 +0530

    add: description sec

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../components/Header";
 import Steps from "../components/Steps";
+import Descripion from "../components/Description";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       {" "}
       <Header />
       <Steps />
+      <Descripion />
     </div>
   );
 };
